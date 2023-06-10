3ff252ffc54daa0bc428d9bc93ee62a800e90ac2
Mohamed Elwakeel <mo7mdhassan7@gmail.com>
2023-06-10 18:22:47 +0300

    done withe the addTask component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import logo from "./logo.svg";
 import "./App.css";
+import React from "react";
+import { AddTask } from "./Components/AddTask";
+import {} from "./Components/ListTask";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">My Tasks</header>
+      <AddTask />
     </div>
   );
 }
 
 export default App;
+
+// The user should be:
+// Able to add a newTodo
+// Filter the tasks by (done/not)
+// Edit a task
