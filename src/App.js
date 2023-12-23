08ff800adf8114c2ea3cfcb316bd821a12e8c5e2
Mohamed-Elwakeel <mo7mdhassan7@gmail.com>
2023-12-23 23:55:09 +0200

    added the footer element to app.js

--- src/App.js
+++ src/App.js
@@ -6,9 +6,16 @@ import { ListTask } from "./Components/ListTask";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Todo App</header>
-      <AddTask />
-      <ListTask />
+      <div className="todoCard">
+        <header className="App-header">Todo App</header>
+        <AddTask />
+        <ListTask />
+      </div>
+      <div className="appInfo">
+        <h4>Double Click to Edit a task</h4>
+        <div>Created with: React js, material ui, css3, & Redux toolkit</div>
+        <div>Hnadcoded by: Mohamed Elwakeel</div>
+      </div>
     </div>
   );
 }
