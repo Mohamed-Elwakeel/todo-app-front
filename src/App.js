8d1e9fbb10760b3efae8c10b103218542f9ecb12
mohamed hassan <mo7mdhassan7@gmail.com>
2023-12-24 17:48:01 +0200

    added the dark and light icon with the state to toggle it and the styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 import { AddTask } from "./Components/AddTask/AddTask";
 import { ListTask } from "./Components/ListTask";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
     <div className="App">
-      <div className="todoCard">
-        <header className="App-header">Todo App</header>
+      <div className={`todoCard ${darkMode ? "dark-mode" : "light-mode"}`}>
+        <div className="App-header">
+          <header>Todo App</header>
+          {darkMode ? (
+            <LightModeIcon className="iconLight" onClick={toggleDarkMode} />
+          ) : (
+            <DarkModeIcon className="iconDark" onClick={toggleDarkMode} />
+          )}
+        </div>
         <AddTask />
         <ListTask />
       </div>
       <div className="appInfo">
         <h4>Double Click to Edit a task</h4>
         <div>Created with: React js, material ui, css3, & Redux toolkit</div>
-        <div>Hnadcoded by: Mohamed Elwakeel</div>
+        <div>Coded by: Mohamed Elwakeel</div>
       </div>
     </div>
   );
 }
 
 export default App;
-
