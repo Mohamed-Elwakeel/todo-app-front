3181f9c9aaa6fee63096fa49b7e2fde2b01d5e4e
mohamed hassan <mo7mdhassan7@gmail.com>
2024-01-10 10:37:33 +0200

    app.js implementing the route to the app.js and home page adding the route

--- src/App.js
+++ src/App.js
@@ -1,39 +1,20 @@
 import "./App.css";
-import React, { useState } from "react";
-import { AddTask } from "./Components/AddTask/AddTask";
-import { ListTask } from "./Components/ListTask";
-import LightModeIcon from "@mui/icons-material/LightMode";
-import DarkModeIcon from "@mui/icons-material/DarkMode";
-import CardFooter from "./Components/CardFooter/CardFooter";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginPage from "./Components/LoginPage/LoginPage";
+import RegistrationPage from "./Components/RegistrationPage/RegistrationPage";
+import HomePage from "./Pages/HomePage";
 
 function App() {
-  const [darkMode, setDarkMode] = useState(false);
-
-  const toggleDarkMode = () => {
-    setDarkMode(!darkMode);
-  };
 
   return (
-    <div className="App">
-      <div className={`todoCard ${darkMode ? "dark-mode" : "light-mode"}`}>
-        <div className="App-header">
-          <header>Todo App</header>
-          {darkMode ? (
-            <LightModeIcon className="iconLight" onClick={toggleDarkMode} />
-          ) : (
-            <DarkModeIcon className="iconDark" onClick={toggleDarkMode} />
-          )}
-        </div>
-        <AddTask />
-        <ListTask />
-        <CardFooter />
-      </div>
-      <div className="appInfo">
-        <h4>Double Click to Edit a task</h4>
-        <div>Created with: React js, material ui, css3, & Redux toolkit</div>
-        <div>Coded by: Mohamed Elwakeel</div>
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegistrationPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { AddTask } from "../Components/AddTask/AddTask";
+import { ListTask } from "../Components/ListTask";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import CardFooter from "../Components/CardFooter/CardFooter";
+
+const HomePage = () => {
+    const [darkMode, setDarkMode] = useState(false);
+
+    const toggleDarkMode = () => {
+        setDarkMode(!darkMode);
+    };
+
+    return (
+        <div className="App">
+            <div className={`todoCard ${darkMode ? "dark-mode" : "light-mode"}`}>
+                <div className="App-header">
+                    <header>Todo App</header>
+                    {darkMode ? (
+                        <LightModeIcon className="iconLight" onClick={toggleDarkMode} />
+                    ) : (
+                        <DarkModeIcon className="iconDark" onClick={toggleDarkMode} />
+                    )}
+                </div>
+                <AddTask />
+                <ListTask />
+                <CardFooter />
+            </div>
+            <div className="appInfo">
+                <h4>Double Click to Edit a task</h4>
+                <div>Created with: React JS, Material UI, CSS3, & Redux toolkit</div>
+                <div>Coded by: Mohamed Elwakeel</div>
+            </div>
+        </div>
+    )
+}
+
+export default HomePage
