64e0d638dbd6fbf8c1af2f9fd65aa5f51439f3d4
mohamed hassan <mo7mdhassan7@gmail.com>
2024-01-15 12:08:09 +0200

    minor changes comment unwanted lines for now

--- src/App.js
+++ src/App.js
@@ -1,35 +1,34 @@
+import React from "react";
 import "./App.css";
-import React, { useState } from "react";
 import { AddTask } from "./Components/AddTask/AddTask";
-import { ListTask } from "./Components/ListTask";
-import LightModeIcon from "@mui/icons-material/LightMode";
-import DarkModeIcon from "@mui/icons-material/DarkMode";
 import CardFooter from "./Components/CardFooter/CardFooter";
+import { ListTask } from "./Components/ListTask";
 
 function App() {
-  const [darkMode, setDarkMode] = useState(false);
+  // const [darkMode, setDarkMode] = useState(false);
 
-  const toggleDarkMode = () => {
-    setDarkMode(!darkMode);
-  };
+  // const toggleDarkMode = () => {
+  //   setDarkMode(!darkMode);
+  // };
 
   return (
     <div className="App">
-      <div className={`todoCard ${darkMode ? "dark-mode" : "light-mode"}`}>
+      {/* <div className={`todoCard ${darkMode ? "dark-mode" : "light-mode"}`}> */}
+      <div className="todoCard" >
         <div className="App-header">
           <header>Todo App</header>
-          {darkMode ? (
+          {/* {darkMode ? (
             <LightModeIcon className="iconLight" onClick={toggleDarkMode} />
           ) : (
             <DarkModeIcon className="iconDark" onClick={toggleDarkMode} />
-          )}
+          )} */}
         </div>
         <AddTask />
         <ListTask />
         <CardFooter />
       </div>
       <div className="appInfo">
-        <h4>Double Click to Edit a task</h4>
+        {/* <h4>Double Click to Edit a task</h4> */}
         <div>Created with: React js, material ui, css3, & Redux toolkit</div>
         <div>Coded by: Mohamed Elwakeel</div>
       </div>
