2710f3ead627e3697d8db13591c81f0d378c71e0
mohamed hassan <mo7mdhassan7@gmail.com>
2024-01-15 12:34:08 +0200

    removing react import duplication

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
-import "./App.css";
-import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
 import LoginPage from "./Components/LoginPage/LoginPage";
 import RegistrationPage from "./Components/RegistrationPage/RegistrationPage";
 import HomePage from "./Pages/HomePage";
